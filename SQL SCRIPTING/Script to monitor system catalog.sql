6db90e34f03626376550ce3ef49562325554082d
satyajit maitra <50560424+MachineLearningWithHuman@users.noreply.github.com>
2020-03-28 00:55:03 +0530

    First phase of work
    system catalog updated

---
+++ SQL SCRIPTING/Script to monitor system catalog.sql
@@ -0,0 +1,168 @@
+--How do I find all the tables that do not have a clustered index in a specified database
+
+SELECT SCHEMA_NAME(t.schema_id) AS schema_name, t.name AS table_name  
+FROM sys.tables AS t  
+WHERE NOT EXISTS   
+   (  
+     SELECT * FROM sys.indexes AS i  
+     WHERE i.object_id = t.object_id  
+     AND i.type = 1  -- or type_desc = 'CLUSTERED'  
+   )  
+ORDER BY schema_name, table_name;  
+GO
+
+USE AdventureWorksDW2017;  
+GO  
+SELECT SCHEMA_NAME(schema_id) AS schema_name, name AS table_name  
+FROM sys.tables   
+WHERE OBJECTPROPERTY(object_id,'TableHasClustIndex') = 0  
+ORDER BY schema_id, name;  
+GO
+
+--How do I find all the owners of entities contained in a specified schema
+
+SELECT 'OBJECT' AS entity_type  
+    ,USER_NAME(OBJECTPROPERTY(object_id, 'OwnerId')) AS owner_name  
+    ,name   
+FROM sys.objects WHERE SCHEMA_NAME(schema_id) = '<schema_name>'  
+UNION   
+SELECT 'TYPE' AS entity_type  
+    ,USER_NAME(TYPEPROPERTY(SCHEMA_NAME(schema_id) + '.' + name, 'OwnerId')) AS owner_name  
+    ,name   
+FROM sys.types WHERE SCHEMA_NAME(schema_id) = '<schema_name>'   
+UNION  
+SELECT 'XML SCHEMA COLLECTION' AS entity_type   
+    ,COALESCE(USER_NAME(xsc.principal_id),USER_NAME(s.principal_id)) AS owner_name  
+    ,xsc.name   
+FROM sys.xml_schema_collections AS xsc JOIN sys.schemas AS s  
+    ON s.schema_id = xsc.schema_id  
+WHERE s.name = '<schema_name>';  
+GO 
+
+--tables that do not have primary key
+SELECT SCHEMA_NAME(t.schema_id) AS schema_name  
+    ,t.name AS table_name  
+FROM sys.tables t   
+WHERE object_id NOT IN   
+   (  
+    SELECT parent_object_id   
+    FROM sys.key_constraints   
+    WHERE type_desc = 'PRIMARY_KEY_CONSTRAINT' -- or type = 'PK'  
+    );  
+GO
+--tables that dont have index
+SELECT SCHEMA_NAME(schema_id) AS schema_name  
+    ,name AS table_name  
+FROM sys.tables   
+WHERE OBJECTPROPERTY(object_id,'IsIndexed') = 0  
+ORDER BY schema_name, table_name;  
+GO 
+
+
+--tables that have identity column
+SELECT SCHEMA_NAME(schema_id) AS schema_name  
+    , t.name AS table_name  
+    , c.name AS column_name  
+FROM sys.tables AS t  
+JOIN sys.identity_columns c ON t.object_id = c.object_id  
+ORDER BY schema_name, table_name;  
+GO  
+
+
+--data types of columns in a tables
+SELECT c.name AS column_name  
+    ,c.column_id  
+    ,SCHEMA_NAME(t.schema_id) AS type_schema  
+    ,t.name AS type_name  
+    ,t.is_user_defined  
+    ,t.is_assembly_type  
+    ,c.max_length  
+    ,c.precision  
+    ,c.scale  
+FROM sys.columns AS c   
+JOIN sys.types AS t ON c.user_type_id=t.user_type_id  
+WHERE c.object_id = OBJECT_ID('dbo.AdventureWorks')  
+ORDER BY c.column_id;  
+GO  
+
+--dependency on a specified function
+
+SELECT OBJECT_NAME(object_id) AS referencing_object_name  
+    ,COALESCE(COL_NAME(object_id, column_id), '(n/a)') AS referencing_column_name  
+    ,*  
+FROM sys.sql_dependencies  
+WHERE referenced_major_id = OBJECT_ID('<schema_name.function_name>')  
+ORDER BY OBJECT_NAME(object_id), COL_NAME(object_id, column_id);  
+GO  
+
+--All stored procedures
+SELECT name AS procedure_name   
+    ,SCHEMA_NAME(schema_id) AS schema_name  
+    ,type_desc  
+    ,create_date  
+    ,modify_date  
+FROM sys.procedures;  
+GO  
+
+--user defined functions
+SELECT name AS function_name   
+  ,SCHEMA_NAME(schema_id) AS schema_name  
+  ,type_desc  
+  ,create_date  
+  ,modify_date  
+FROM sys.objects  
+WHERE type_desc LIKE '%FUNCTION%';  
+GO  
+
+
+--views in a database
+SELECT name AS view_name   
+  ,SCHEMA_NAME(schema_id) AS schema_name  
+  ,OBJECTPROPERTYEX(object_id,'IsIndexed') AS IsIndexed  
+  ,OBJECTPROPERTYEX(object_id,'IsIndexable') AS IsIndexable  
+  ,create_date  
+  ,modify_date  
+FROM sys.views;
+
+--modyfiied in last 1095 days
+SELECT name AS object_name   
+  ,SCHEMA_NAME(schema_id) AS schema_name  
+  ,type_desc  
+  ,create_date  
+  ,modify_date  
+FROM sys.objects  
+WHERE modify_date > GETDATE() - 1095
+ORDER BY modify_date;  
+GO 
+--definition of a module
+SELECT definition  
+FROM sys.sql_modules  
+WHERE object_id = OBJECT_ID('<schema_name.object_name>');  
+GO  
+
+--primary key
+SELECT i.name AS index_name  
+    ,COL_NAME(ic.object_id,ic.column_id) AS column_name  
+    ,ic.index_column_id  
+    ,key_ordinal  
+FROM sys.indexes AS i  
+INNER JOIN sys.index_columns AS ic   
+    ON i.object_id = ic.object_id AND i.index_id = ic.index_id  
+WHERE i.is_primary_key = 1   
+    AND i.object_id = OBJECT_ID('<schema_name.table_name>');  
+GO
+
+
+--finding all tbale that are partitioned
+SELECT SCHEMA_NAME(o.schema_id) AS schema_name  
+    ,OBJECT_NAME(p.object_id) AS table_name  
+    ,i.name AS index_name  
+    ,p.partition_number  
+    ,rows   
+FROM sys.partitions AS p  
+INNER JOIN sys.indexes AS i ON p.object_id = i.object_id AND p.index_id = i.index_id  
+INNER JOIN sys.partition_schemes ps ON i.data_space_id=ps.data_space_id  
+INNER JOIN sys.objects AS o ON o.object_id = i.object_id  
+ORDER BY index_name, partition_number;  
+GO 
+  
